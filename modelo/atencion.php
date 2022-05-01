3f0cd015b9fefe266df99b71ba4ed64271883c99
Eluney Salvaro <eluneyjsalvaro@gmail.com>
2022-05-01 20:41:15 -0300

    getters and setters from atencion

--- modelo/atencion.php
+++ modelo/atencion.php
@@ -26,5 +26,62 @@ class atencion
     }
 
 
+    //Setters and Getters
+    public function getIdAtencion(){
+        return $this->idAtencion;
+    }
+    public function setIdAtencion($idAtencion){
+        $this->idAtencion = $idAtencion;
+    }
+    public function getObjPeluquero(){
+        return $this->objPeluquero;
+    } 
+    public function setObjPeluquero($objPeluquero){
+        $this->objPeluquero = $objPeluquero;
+    }
+    public function getObjServicio(){
+        return $this->objServicio;
+    }
+    public function setObjServicio($objServicio){
+        $this->objServicio = $objServicio;
+    }
+    public function getObjCliente(){
+        return $this->objCliente;
+    }
+    public function setObjCliente($objCliente){
+        $this->objCliente = $objCliente;
+    }
+    public function getFechaAtencion(){
+        return $this->fechaAtencion;
+    }
+    public function setFechaAtencion($fechaAtencion){
+        $this->fechaAtencion = $fechaAtencion;;
+    }
+    public function getHoraAtencion(){
+        return $this->horaAtencion;
+    }
+    public function setHoraAtencion($horaAtencion){
+        $this->horaAtencion = $horaAtencion;
+    }
+    public function getMetodoPago(){
+        return $this->metodoPago;
+    }
+    public function setMetodoPago($metodoPago){
+        $this->metodoPago = $metodoPago;
+    }
+    public function getCortePagadoPeluquero(){
+        return $this->cortePagadoPeluquero;
+    }
+    public function setCortePagadoPeluquero($cortePagadoPeluquero){
+        $this->cortePagadoPeluquero = $cortePagadoPeluquero;
+    }
+    public function getMensajeOperacion(){
+        return $this->mensajeOperacion;
+    }
+    public function setMensajeOperacion($mensajeOperacion){
+        $this->mensajeOperacion = $mensajeOperacion;
+    }
 
+
+    
 }
